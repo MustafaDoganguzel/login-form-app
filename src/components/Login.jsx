35ecb6235105b2d46f66a54df43a86c41e135282
oznurtopcu <topcuoznur121@gmail.com>
2024-12-28 00:48:18 +0300

    add success component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -94,9 +94,8 @@ export default function Login() {
         );
         if (user) {
           setForm(initialForm);
-          history.push('/main');
+          history.push('/success');
         } else {
-          history.push('/error');
         }
       });
   };
